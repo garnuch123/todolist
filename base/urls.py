e503096557829eb3058a43251ac47cfdf417bf27
Kuba <jakub.oberszt@gmail.com>
2022-08-28 12:36:35 +0200

    make it bigger

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='tasks'), name='logout'),
+
+    path('', Tasklist.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+]
